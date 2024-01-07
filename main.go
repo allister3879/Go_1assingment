827cdf89d05211be79b754d6bd199e714885f3ce
dias <lbrjyz05@Mail.ru>
2024-01-07 16:07:13 +0600

    4

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type JsonRequest struct {
-	Message string `json:"message"`
+type RegistrationRequest struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Phone     string `json:"phone"`
+	Product   string `json:"product"`
+	Date      string `json:"date"`
+	Time      string `json:"time"`
 }
 
 type JsonResponse struct {
@@ -19,34 +25,50 @@ type JsonResponse struct {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlePost).Methods("POST")
+	r.HandleFunc("/register", handleRegister).Methods("POST")
+	r.HandleFunc("/registration", serveRegistrationPage).Methods("GET")
 
 	fmt.Println("Server listening on :8080")
 	http.ListenAndServe(":8080", r)
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
-	var requestData JsonRequest
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the homepage!")
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	var requestData RegistrationRequest
 
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&requestData)
 	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
 		return
 	}
 
-	if requestData.Message == "" {
-		http.Error(w, "Invalid JSON message", http.StatusBadRequest)
+	// Check for required fields
+	if requestData.Email == "" || requestData.FirstName == "" || requestData.LastName == "" ||
+		requestData.Phone == "" || requestData.Product == "" || requestData.Date == "" || requestData.Time == "" {
+		response := JsonResponse{
+			Status:  "400",
+			Message: "Missing required fields in the registration data",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 		return
 	}
 
-	fmt.Println("Received message:", requestData.Message)
+	// Process registration logic here
 
 	response := JsonResponse{
 		Status:  "success",
-		Message: "Data successfully received",
+		Message: "Appointment booked successfully",
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+}
+
+func serveRegistrationPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "Registration.html")
 }
